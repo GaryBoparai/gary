34df77116d3cc4728cccf707b823e63f97beb86e
Gurnoor singh boaprai <bopagurn@sheridancollege.ca>
2022-02-06 18:45:21 -0500

    new repo for student code

---
+++ cardgame/src/cardgame/cardtrick.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cardgame;
+
+/**
+ *
+ * @author Human
+ */
+public class cardtrick extends card{
+      private String playername;
+
+    public CardTrick(String playername, Value value, Suit suit) {
+        super(value, suit);
+        this.playername = playername;
+    }
+
+    public String getPlayername() {
+        return playername;
+    }
+
+    public void setPlayername(String playername) {
+        this.playername = playername;
+    }
+}
